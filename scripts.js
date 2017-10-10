768a4bea846a180049660c1fed633928fa31bba8
olddonny <reedtrekks@gmail.com>
2017-10-10 11:08:21 -0500

    dice roll and sum

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 
 var generate = document.getElementById('die');
 var rollDie = document.getElementById('roll');
-
+var sum = document.getElementById('sum');
 let dieArray = [];
 
-
+//push new die into array
 document.addEventListener('DOMContentLoaded', function () {
     generate.addEventListener('click', function () {
         dieArray.push(new die());
     });
 });
-
+//push new die into array
 
 
 //die constructer 
@@ -21,32 +21,49 @@ var die = function () {
     this.div.innerText = this.value;
     document.body.appendChild(this.div);
     this.div.addEventListener('click', this.change.bind(this));
+    this.div.addEventListener('dblclick', this.removeDie.bind(this));
 };
 //die constructer 
 
 
-function random() {
-    return Math.floor(Math.random() * (7 - 0)) + 0;
-}
-
+//prototype for on click die flip
 die.prototype.change = function () {
     var randNum = random();
     this.div.innerText = randNum;
     this.value = randNum;
 };
+//prototype for on click die flip
+
+//prototype remove
+die.prototype.removeDie = function () {
+    var current = dieArray.indexOf(this);
+    dieArray.splice(current, 1);
+    this.div.remove();
+};
+//prototype remove
+
+//random die
+function random() {
+    return Math.floor(Math.random() * (6 - 0)) + 1;
+}
+//random die
 
 
-
-rollDie.addEventListener('click', function(){
+//roll die button and loop
+rollDie.addEventListener('click', function () {
     var arrayLength = dieArray.length;
     for (var i = 0; i < arrayLength; i++) {
-        
-        
-        
+        var dieNum = dieArray[i];
+        dieNum.value = random();
+        dieNum.div.innerText = dieNum.value;
     }
 });
+//roll die button and loop
 
-
-
-
-console.log(dieArray);
+//sum all dice
+sum.addEventListener('click', function () {
+    var total = dieArray.reduce(function (acc, curr) {
+        return acc + curr.value;
+    }, 0); alert(`total of all die on screen is ${total}`)
+});
+//sum all dice
